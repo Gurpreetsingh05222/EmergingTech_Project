4653dfdbef115080c6e47fc45d8a97f122d5ccc5
kriti269 <kriti269@gmail.com>
2021-04-12 18:11:53 -0400

    -added sign up

---
+++ DialogDiaries/blog/static/js/sign_in.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+    $(".isa_error").delay(1000).fadeOut(5000);
+    $(".isa_success").delay(1000).fadeOut(5000);
+    document.getElementsByClassName("validation-error")[0].innerHTML = '';
+    $(".validation-error").hide();
+    $(".btn-register").click(function(){
+        if($(".first-name-container, .last-name-container, .email-container, .password2-container").is(":visible")==false){
+            $(".first-name-container, .last-name-container, .email-container, .password2-container").css({'display':'flex'});
+            $(".sign-in-bg").css("height","725px")
+        }
+        else{
+            var error_message = "";
+            var firstname = $(".first-name-container input").val();
+            var lastname = $(".last-name-container input").val();
+            var email = $(".email-container input").val();
+            var password2 = $(".password2-container input").val();
+            var password1 = $(".password-container input").val();
+            var username = $(".user-name-container input").val();
+            if(firstname!="" && lastname!="" && email!="" && password1!="" && password2!="" && username!=""){
+                if(password2==password1){
+                    $(".contact-form").submit();
+                }
+                else{
+                    error_message = "Password does not match!"
+                }
+            }
+            else{
+                error_message = "Please fill out the entire form!"
+            }
+            if(error_message!=""){
+                var child = document.createTextNode(error_message);
+                document.getElementsByClassName("validation-error")[0].appendChild(child);
+                $(".validation-error").show();
+                $(".validation-error").delay(1000).fadeOut(5000, function(){
+                    document.getElementsByClassName("validation-error")[0].innerHTML = '';
+                });
+
+            }
+
+        }
+
+    });
+
+});
--- DialogDiaries/blog/views.py
+++ DialogDiaries/blog/views.py
@@ -1,7 +1,9 @@
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.template import loader
 from django.views import generic
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.forms import UserCreationForm
 
 from .models import Post, User
 from django.contrib.auth import authenticate, login, logout
@@ -23,14 +25,32 @@ class GetSignIn(generic.TemplateView):
 @csrf_exempt
 class GetUserView(generic.TemplateView):
     def LogInUser(request):
-        username = request.POST['username']
-        password = request.POST['password']
+        username = request.POST.get('username')
+        password = request.POST.get('password1')
+        email = request.POST.get('email')
+        context = {'login_error' : None, 'register_error': None, 'register_success': None}
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
             return redirect('/', {'post_list': Post.objects.order_by('-created_on')})
         else:
-            return redirect('/sign-in/')
+            template = loader.get_template('sign_in.html')
+            if email!="":
+                print(request.POST)
+                form = UserCreationForm(request.POST)
+                if form.is_valid():
+                    form.save()
+                    user = authenticate(request, username=username, password=password)
+                    print(user)
+                    if user is not None:
+                        context = {'register_success': 'Registered successfully! Log in now!'}
+                    else:
+                        context = {'register_error': 'Invalid details! Unable to sign up!'}
+                else:
+                    context = {'register_error': 'Invalid details! Unable to sign up!'}
+            else:
+                context = {'login_error': 'Username or Password is invalid!'}
+            return HttpResponse(template.render(context, request))
 
     def LogOut(request):
         logout(request)
