712abe04a5e955b3a01a070862d74888e32f87ca
kriti269 <kriti269@gmail.com>
2021-04-11 22:17:54 -0400

    -added sign in and sign out functionalities

--- DialogDiaries/blog/views.py
+++ DialogDiaries/blog/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, redirect
 from django.views import generic
-from .models import Post
+from django.views.decorators.csrf import csrf_exempt
+
+from .models import Post, User
+from django.contrib.auth import authenticate, login, logout
 
 
 class GetAllPosts(generic.ListView):
@@ -10,3 +14,25 @@ class GetAllPosts(generic.ListView):
 class GetPostDetails(generic.DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+
+class GetSignIn(generic.TemplateView):
+    model = User
+    template_name = 'sign_in.html'
+
+@csrf_exempt
+class GetUserView(generic.TemplateView):
+    def LogInUser(request):
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/', {'post_list': Post.objects.order_by('-created_on')})
+        else:
+            return redirect('/sign-in/')
+
+    def LogOut(request):
+        logout(request)
+        return redirect('/')
+
