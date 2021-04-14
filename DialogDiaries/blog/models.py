a9e01ce20599b029bac49d566fc49260edbf5775
gurpreetsingh05222 <gurpreetsingh05222@gmail.com>
2021-04-13 22:56:07 -0400

    form validation error, design and create post link in navbar

--- DialogDiaries/blog/models.py
+++ DialogDiaries/blog/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.shortcuts import redirect
+from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
